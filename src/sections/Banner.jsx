bfcb3f923735578bce586762c4c478a57d38a2c8
Riajul-56 <riajulhasansiam@gmail.com>
2025-02-19 23:53:39 +0600

    bannere responsive

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -8,19 +8,23 @@ const Banner = () => {
   return (
     <section className="pt-[53px] relative z-50">
       <Container>
-        <Flex className="items-center">
-          <div className="max-w-[785px]">
-            <h1 className="font-black tracking-wider text-white text-8xl banner_heading">
+        <Flex className="items-center flex-col sm:flex-row">
+          <div className="max-w-[785px] order-1 sm:order-0">
+            <h1 className="font-black tracking-wider text-white text-4xl text-center sm:text-left sm:text-8xl banner_heading">
               Best NFTs Marketplace.
             </h1>
-            <p className="text-xl mt-8 mb-[60px] leading-[183%] max-w-[765px]">
+            <p className="text-xl mt-8 mb-[60px] leading-[183%] max-w-[765px] text-center sm:text-left">
               Ultrices eget pretium sit euismod mi id. In commodo auctor eget
               congue sit. Risus, aliquam odio posuere ac in in nisl sed augue.
               Porta aenean egestas malesuada in pulvinar enim viverra.
             </p>
-            <Flex className="gap-[17px]">
-              <Button text={"Get started"} />
-              <Button text={"Create NFTs"} bgShow={false} />
+            <Flex className="gap-[17px] flex-col sm:flex-row">
+              <Button text={"Get started"} className="text-center" />
+              <Button
+                text={"Create NFTs"}
+                bgShow={false}
+                className="text-center"
+              />
             </Flex>
           </div>
           <Flex className="rounded-full max-w-[750px] max-h-[750px] overflow-hidden justify-center bg-[rgb(134,155,223,.08)] border-4 border-[#17152c] relative">
