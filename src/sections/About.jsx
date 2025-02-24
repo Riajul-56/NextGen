166045301bae444bf0cb0b5855e1f5a1074ed1d2
Riajul-56 <riajulhasansiam@gmail.com>
2025-02-25 00:56:27 +0600

    About section responsive

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,21 +7,21 @@ import aboutshade from "../assets/about_shade.png";
 
 const About = () => {
   return (
-    <section className="mt-[200px] mb-[200px] relative">
+    <section className="lg:mt-[200px] mt-[100px] mb-[200px] relative px-2 MyXl:px-0">
       <img src={aboutshade} className="absolute top-[500px] left-0" />
       <Container>
-        <Flex className="gap-[49px] items-center">
+        <Flex className="gap-[49px] items-center flex-col lg:flex-row">
           <div className="z-50">
             <img src={aboutImg} />
           </div>
 
-          <div className="max-w-[680px]">
-            <h2 className="font-black text-[56px] leading-[121%] text-text_title">
+          <div className="xl:max-w-[680px] lg:max-w-[50%]">
+            <h2 className="font-black xl:text-[56px] text-4xl text-center lg:text-left leading-[121%] text-text_title">
               Subscribe And get Latest news update about NFTs.
             </h2>
-            <p className="text-lg font-normal leading-[183%] mt-[35px] mb-15">
+            <p className="text-lg font-normal leading-[183%] mt-[35px] mb-15 text-center lg:text-left">
               In commodo auctor eget congue sit. Ultrices eget pretium sit
-              euismod mi id. Risus, aliquam odio posuere ac in in nisl sed
+              euilgod mi id. Risus, aliquam odio posuere ac in in nisl sed
               augue. Porta aenean egestas malesuada in pulvinar enim viverra.
             </p>
             <form className="relative ">
